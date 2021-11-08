ff801aa0e92ab96754cc34bd84cec6ee2423d3b8
= <=>
2021-11-07 21:32:09 -0300

    Setup do microservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
+import { RankingsModule } from './rankings/rankings.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const connectionParams : object = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    MongooseModule.forRoot(
+      'mongodb+srv://' +
+      `${process.env.DB_USER}:` +
+      `${process.env.DB_PASS}@` +
+      `${process.env.DB_HOST}/` +
+      `${process.env.DB_NAME}?retryWrites=true&w=majority`
+      , connectionParams),
+    RankingsModule
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+
+let amqpUrl = 'amqp://';
+amqpUrl += `${process.env.RABBITMQ_USER}:`;
+amqpUrl += process.env.RABBITMQ_PASS;
+amqpUrl += `@${process.env.RABBITMQ_HOST}`;
+amqpUrl += `:${process.env.RABBITMQ_PORT}/smartranking`;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [amqpUrl],
+      noAck: false,
+      queue: 'rankings'
+    }
+  });
+
+  await app.listen();
 }
 bootstrap();
